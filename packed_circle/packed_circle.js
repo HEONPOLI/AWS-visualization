2107e3c1fd722350c0776b3be0c6dec3a64f2fb1
_PeterParker <jkshin0903@gmail.com>
2021-04-30 23:30:47 +0900

    updated

--- packed_circle/packed_circle.js
+++ packed_circle/packed_circle.js
@@ -1,20 +1,29 @@
-const svg = d3.select("body").select("svg");
-const margin = 20;
-const diameter = +svg.attr("width");
-const g = svg
-  .append("g")
-  .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+const MARGIN = 20;
+const ANGLE = 78.5;
+const SPAN = 750;
+const OPACITY = 0.14;
 
-// const color = d3
-//   .scaleLinear()
-//   .domain([-1, 5])
-//   .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
-//   .interpolate(d3.interpolateHcl);
+const svg = d3.select("body").select("svg");
+const diameter = +svg.attr("width");
+const nodeGroup = svg
+  .append("g")
+  .attr("class", "nodeGroup")
+  .attr("transform", `translate(${diameter / 2}, ${diameter / 2})`);
+
+const color = d3
+  .scaleLinear()
+  .domain([-1, 5])
+  .range(["hsl(152,80%,80%)", "hsl(228,30%,40%)"])
+  .interpolate(d3.interpolateHcl);
 
 const pack = d3
   .pack()
-  .size([diameter - margin, diameter - margin])
+  .size([diameter - MARGIN, diameter - MARGIN])
   .padding(8);
+
+const dd = d3.select("body").select("select");
+
+//<<<================== init DEFINITION ==================>>>
 
 const init = function (root) {
   root = d3
@@ -23,12 +32,10 @@ const init = function (root) {
     .sort((a, b) => b.value - a.value);
 
   let focus = root;
-  // console.log(root.children.length);
-  // const nodes = pack(root).descendants();
-  const nodes = pack(root);
+  const nodes = pack(root).descendants();
   let view;
 
-  const circle = g
+  const circle = nodeGroup
     .selectAll("circle")
     .data(nodes)
     .enter()
@@ -40,9 +47,9 @@ const init = function (root) {
           : "node node--leaf"
         : "node node--root";
     })
-    .style("display", function (d) {
-      return d.data.name === "null" ? "none" : "inline";
-    })
+    .style("display", (d) => (d.height === 0 ? "none" : "inline"))
+    .style("fill", (d) => color(d.depth))
+    .style("opacity", (d) => (d.depth > 1 ? OPACITY : 1))
     .on("click", function (event, d) {
       if (d.height === 1) {
         // zoom(root);
@@ -55,7 +62,7 @@ const init = function (root) {
       }
     });
 
-  const image = g
+  const image = nodeGroup
     .selectAll("image")
     .data(nodes)
     .enter()
@@ -66,13 +73,30 @@ const init = function (root) {
     .attr("width", (d) => d.r / 2)
     .attr("height", (d) => d.r / 2)
     .attr("href", (d) => d.data.href)
-    .style("display", function (d) {
-      return d.parent === root ? "inline" : "none";
-    });
+    .style("opacity", (d) => (d.depth > 1 ? 0 : 1))
+    .style("display", (d) => (d === root ? "none" : "inline"));
 
-  const node = g.selectAll("circle, image");
+  const node = nodeGroup.selectAll("circle, image");
 
-  zoomTo([root.x, root.y, root.r * 2 + margin]);
+  zoomTo([root.x, root.y, root.r * 2 + MARGIN]);
+
+  dd.on("change", function (event) {
+    const val = event.target.value;
+
+    nodeGroup
+      .selectAll("circle")
+      .style("opacity", (d) =>
+        d.data.name.startsWith(val) ? 1 : d !== root ? OPACITY : null
+      );
+
+    nodeGroup
+      .selectAll("image")
+      .style("opacity", (d) =>
+        d.data.name.startsWith(val) ? 1 : d !== root ? 0 : null
+      );
+  });
+
+  //<<<================== zoom DEFINITION ==================>>>
 
   function zoom(d) {
     const focus0 = focus;
@@ -80,50 +104,103 @@ const init = function (root) {
 
     const transition = d3
       .transition()
-      .duration(750)
+      .duration(SPAN)
       .tween("zoom", function (d) {
         const i = d3.interpolateZoom(view, [
           focus.x,
           focus.y,
-          focus.r * 2 + margin,
+          focus.r * 2 + MARGIN,
         ]);
-        return function (t) {
-          zoomTo(i(t));
-        };
+        return (t) => zoomTo(i(t));
       });
 
     transition
       .selectAll("image")
-      .filter(function (d) {
-        return d.parent === focus || this.style.display === "inline";
-      })
+      .filter((d) => focus.descendants().includes(d))
+      .style("opacity", (d) => (d.parent === focus ? 1 : 0));
+
+    transition
+      .selectAll("circle")
+      .filter((d) => focus.descendants().includes(d))
       .style("opacity", function (d) {
-        return d.parent === focus ? 1 : 0;
-      })
-      .on("start", function (d) {
-        if (d.parent === focus) this.style.display = "inline";
-      })
-      .on("end", function (d) {
-        if (d.parent !== focus) this.style.display = "none";
+        if (focus.depth + 1 < d.depth) {
+          return OPACITY;
+        } else if (focus.depth + 1 === d.depth) {
+          return 1;
+        }
       });
   }
+
+  //<<<================== zoomTo DEFINITION ==================>>>
 
   function zoomTo(v) {
     const k = diameter / v[2];
     view = v;
 
     node.attr("transform", function (d) {
-      return "translate(" + (d.x - v[0]) * k + "," + (d.y - v[1]) * k + ")";
+      return `translate(${(d.x - v[0]) * k}, ${(d.y - v[1]) * k})`;
     });
 
-    circle.attr("r", function (d) {
-      return d.r * k;
-    });
+    circle.attr("r", (d) => d.r * k);
 
-    image.attr("y", (d) => (-d.r / 5) * k);
-    image.attr("x", (d) => (-d.r / 5) * k);
-    image.attr("width", (d) => (d.r / 2.5) * k);
-    image.attr("height", (d) => (d.r / 2.5) * k);
+    image.attr("y", (d) => (-d.r / 4) * k);
+    image.attr("x", (d) => (-d.r / 4) * k);
+    image.attr("width", (d) => (d.r / 2) * k);
+    image.attr("height", (d) => (d.r / 2) * k);
+
+    appendLabel(v, k);
+  }
+
+  //<<<================== appendLabel DEFINITION ==================>>>
+
+  function appendLabel(v, k) {
+    d3.selectAll(".labelGroup").remove();
+
+    const labelGroup = svg
+      .append("g")
+      .attr("class", "labelGroup")
+      .attr("transform", `translate(${diameter / 2}, ${diameter / 2})`);
+
+    const path = labelGroup
+      .selectAll("path")
+      .data(nodes)
+      .enter()
+      .append("path")
+      .attr("id", (d, i) => `circleArc_${i}`)
+      .style("fill", "none")
+      .attr("d", function (d, i) {
+        return `M ${-d.r * k} 0 A ${d.r * k} ${d.r * k} 0 0 1 ${d.r * k} 0`;
+      })
+      .attr("transform", function (d, i) {
+        return `translate(${
+          (d.x - v[0]) * k
+        }, ${(d.y - v[1]) * k})rotate(${ANGLE})`;
+      });
+
+    const text = labelGroup
+      .selectAll("text")
+      .data(nodes)
+      .enter()
+      .append("text")
+      .attr("class", "circleText")
+      .style("font-size", (d) => Math.round((d.r / 7) * k) + "px")
+      .style("text-anchor", "start")
+      .style("display", function (d) {
+        // d.height === 0 || d === root ? "none" : "inline";
+        if (d.height === 0 || d === root || focus.depth + 1 < d.depth) {
+          return "none";
+        } else if (focus.depth + 1 === d.depth) {
+          return "inline";
+        }
+      })
+      .attr("startOffset", "50%")
+      .style("fill-opacity", 0)
+      .append("textPath")
+      .attr("href", (d, i) => `#circleArc_${i}`)
+      .text((d) => d.data.name)
+      .transition()
+      .duration(SPAN / 3)
+      .style("fill-opacity", 1);
   }
 };
 
